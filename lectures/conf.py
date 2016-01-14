383b2f7123cf8c144fd63a7a6ffdf2645d967fc3
cllburn <cliburn.chan@duke.edu>
2016-01-14 13:45:25 -0500

    Remove 'Documentation' text

--- lectures/conf.py
+++ lectures/conf.py
@@ -227,7 +227,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'ComputationalStatisticsinPython.tex', 'Computational Statistics in Python Documentation',
+  (master_doc, 'ComputationalStatisticsinPython.tex', 'Computational Statistics in Python',
    'Cliburn Chan, Janice McCarthy', 'manual'),
 ]
 
@@ -257,7 +257,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'computationalstatisticsinpython', 'Computational Statistics in Python Documentation',
+    (master_doc, 'computationalstatisticsinpython', 'Computational Statistics in Python',
      [author], 1)
 ]
 
@@ -271,7 +271,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'ComputationalStatisticsinPython', 'Computational Statistics in Python Documentation',
+  (master_doc, 'ComputationalStatisticsinPython', 'Computational Statistics in Python',
    author, 'ComputationalStatisticsinPython', 'One line description of project.',
    'Miscellaneous'),
 ]
